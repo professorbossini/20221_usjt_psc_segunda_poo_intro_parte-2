13bcc4840d0d30a9c1ad762985ee877b3518f83b
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-11 21:36:58 -0300

    Teste com sorteio para cada operacao

--- Jogo.java
+++ Jogo.java
@@ -1,5 +1,35 @@
+import java.util.Random;
 public class Jogo {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        Personagem cacador = new Personagem();
+        Random gerador = new Random();
+        /*
+            1: Caçar
+            2: Comer
+            3: Dormir
+        */ 
+        cacador.nome = "John";
+        while (true){
+            int oQueFazer = gerador.nextInt(3) + 1;
+            switch(oQueFazer){
+                case 1:
+                    cacador.cacar();
+                    break;
+                case 2:
+                    cacador.comer();
+                    break;
+                case 3:
+                    cacador.dormir();
+                    break;
+            }
+            System.out.println("=====================================");
+            Thread.sleep(2 * 1000);
+        }
+
+
+        //esse também é um loop infinito
+        // for (; ;){
+
+        // }
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,26 +1,49 @@
 public class Personagem{
     //atributos
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
+    
 
     //métodos
     //um método tem quatro partes:
     //tipo de retorno, nome, lista de parâmetros, corpo
     //isso é a definição de um método
     void cacar(){
-        System.out.println(nome + " caçando...");
+        if (energia >= 2){
+            System.out.println(nome + " caçando...");
+            energia = energia - 2;
+            //energia -= 2;
+        }
+        else{
+            System.out.println(nome + " sem energia para caçar...");
+        }
+        fome = Math.min(fome + 1, 10);
+        sono = Math.min(sono + 1, 10);
+        // if (fome < 10)
+        //     fome += 1;
+
     }
 
     void comer(){
-        System.out.printf("%s comendo...\n", nome);
+        if (fome >= 1){
+            System.out.printf("%s comendo...\n", nome);
+            energia = Math.min(energia + 1, 10);
+            fome -= 1;
+        }
+        else{
+            System.out.println(nome + " sem fome...");
+        }
     }
 
     void dormir(){
-        //System.out.print(nome + " dormindo\n");
-        String exibir = String.format("%s dormindo...\n", nome);
-        System.out.print(exibir);
+        if (sono >= 1){
+            //System.out.print(nome + " dormindo\n");
+            String exibir = String.format("%s dormindo...\n", nome);
+            System.out.print(exibir);
+            energia = energia + 1 <= 10 ? energia + 1 : 10;
+        }
     }
     
 }
